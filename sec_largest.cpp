89b0a7a4b7c66990f4be8ac27e9c43f83e9fb303
Shubham Shelke <162583029+Sip2401g@users.noreply.github.com>
2026-04-13 23:18:01 +0530

    Add files via upload

---
+++ sec_largest.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+int arr[]={1,2,3,6};
+int n = sizeof(arr)/sizeof(arr[0]);
+int largest =0;
+int sec_largest = 0;
+for (int i =0 ; i<n-1 ; i++){
+if (arr[i]>largest){
+    
+    largest = arr[i];
+    sec_largest =largest;
+}
+}
+cout<<sec_largest;
+}
