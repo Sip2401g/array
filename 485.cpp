6f055a7e3d638b78c963ff10c5b9db19e18fe31a
Shubham Shelke <162583029+Sip2401g@users.noreply.github.com>
2026-04-18 10:49:12 +0530

    Add files via upload

---
+++ 485.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,1,1,0,1,1,1,1};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    int count =0;
+    int maxCount=0;
+    for(int i =0;i<n;i++){
+        if(arr[i]==1){
+            count++;
+    maxCount = max(count,maxCount);                
+        }
+        else{
+            count=0;
+        }
+} cout<<maxCount;   
+}
