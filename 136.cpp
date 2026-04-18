175310829d9aa3a64329cabf4075e688e6dd23a1
Shubham Shelke <162583029+Sip2401g@users.noreply.github.com>
2026-04-18 21:57:06 +0530

    Update 136.cpp

--- 136.cpp
+++ 136.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
-#include<set>
 using namespace std;
 int main(){
     int arr[]= {1,1,2,4,6,2,4,5,6};
     int n = sizeof(arr)/sizeof(arr[0]);
-    set<int> s;
-    for(int i=0; i<n; i++){
-        s.insert(arr[i]); 
+ for (int i =0; i<n; i++){
+    int count = 0;
+        for(int j =0; j<n; j++){
+            if(arr[i]==arr[j]){
+                    count ++;
+                        }
     }
-    for(auto it=s.begin(); it!=s.end(); it++){
-        cout<<*it<<" ";
-    }
-    return 0;
-}
+        if(count == 1){
+            cout<<arr[i];
+}}
+     return -1;
+ }
